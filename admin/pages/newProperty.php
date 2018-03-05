9842015edbb78611995c738c69f941fcd1582351
John <mjohnmwangi@gmail.com>
2018-03-05 21:39:12 +0300

    udates

--- admin/pages/newProperty.php
+++ admin/pages/newProperty.php
@@ -114,7 +114,40 @@
 			// Clear the form of Values
 			$_POST['userFirstName'] = $_POST['userLastName'] = $_POST['userEmail'] = $_POST['userEmail_r'] = $_POST['primaryPhone'] = $_POST['altPhone'] = '';
 
-			$msgBox = alertBox($newPropMsg, "<i class='fa fa-check-square'></i>", "success");
+///generate invoice in the payment gateway
+
+
+		$data =
+		array(
+		 "createdBy" => "swiftcloudace",
+		 "invoiceNumber" => $unitName,
+		 "customerRef" => $unitName,
+		 "name" => $propertyName,
+		 "recurring" => "Y",
+		 "remarks" => $propertyName."payments",
+		 "status" => "ACTIVE",
+		 "serviceCode" => "01",
+		 "type" => "1",
+		  "merchantId" => "1002",
+			"amount" => $propertyRate
+		);
+		$data_string = json_encode($data);
+
+		$ch = curl_init('http://localhost:8094/invoice/create/v1');
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type: application/json',
+		    'Content-Length: ' . strlen($data_string))
+		);
+
+		$result = curl_exec($ch);
+
+
+$msgBox = alertBox($newPropMsg, "<i class='fa fa-check-square'></i>", "success");
+
+
 		}
 	}
 
---
+++ ipnAdapter.php
@@ -0,0 +1,111 @@
+<?php
+
+
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
+    throw new Exception('Request method must be POST!');
+}
+
+//Make sure that the content type of the POST request has been set to application/json
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if(strcasecmp($contentType, 'application/json') != 0){
+    throw new Exception('Content type must be: application/json');
+}
+
+
+$data = json_decode(trim(file_get_contents('php://input')), true);
+
+//If json_decode failed, the JSON is invalid.
+if(!is_array($data)){
+    throw new Exception('Received content contained invalid JSON!');
+}
+
+$payment_source =$data->paymentSource;
+$paymentAmount =$data->paymentAmount;
+$paymentRef =$data->paymentRef;
+$paidInByName =$data->paidInByName;
+$payidInByMobile =$data->payidInByMobile;
+$status =$data->status;
+$merchantCode =$data->merchantCode;
+
+$propqry = "SELECT propertyId, propertyName, isLeased FROM properties where unitName ='".$paymentRef."'";
+
+$propres = mysqli_query($mysqli, $propqry) or die('-1'.mysqli_error());
+
+while ($prop = mysqli_fetch_assoc($propres)) {
+  $prodid =$prop['propertyId'];
+
+  $leaseQuery ="SELECT *  FROM leases  where propertyId ='".$prodid."'";
+
+  $leaseres = mysqli_query($mysqli, $leaseQuery) or die('-1'.mysqli_error());
+
+  while ($ls= mysqli_fetch_assoc($leaseres)) {
+
+     $leaseid =$ls['leaseId'];
+
+
+
+$stmt = $mysqli->prepare("
+          INSERT INTO
+            payments(
+              leaseId,
+              propertyId,
+              adminId,
+              userId,
+              hasRefund,
+              paymentDate,
+              amountPaid,
+              paymentFor,
+              paymentType,
+              isRent,
+              rentYear,
+              notes,
+              lastUpdated
+            ) VALUES (
+              ?,
+              ?,
+              '1',
+              '1',
+              '0',
+              NOW(),
+              ?,
+              'rent',
+              ?,
+              '1',
+                NOW(),
+                  ?,
+              NOW(),
+
+            )");
+$stmt->bind_param('sssssssssssssssssss',
+  $leaseid,
+  $prodid,
+  $paymentAmount,
+  $payment_source,
+  $paidInByName."/".$payidInByMobile
+);
+$stmt->execute();
+$stmt->close();
+
+$jsonData = array(
+    'responsecode' => 'OK',
+    'response_message' => 'Succesful'
+);
+
+echo json_encode($jsonData);
+
+  }
+
+}
+
+$jsonData = array(
+    'responsecode' => 'FAILED',
+    'response_message' => 'Failed'
+);
+
+echo json_encode($jsonData);
+
+
+
+
+
+?>
