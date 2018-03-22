8c34d9d17eb2176111c494f44c6c6450b7fddc87
John Senga <johnsenga@gmail.com>
2018-03-22 10:25:35 +0300

    upload properties and leases

---
+++ admin/pages/importOutcome.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'includes/header.php';
+?>
+<div class="container page_block noTopBorder">
+  <hr class="mt-0 mb-0" />
+  <div class="alertMsg succuess">
+  			<div class="msgIcon pull-left">
+  				<i class="fa fa-info-circle"></i>
+  			</div>
+  			Successfully imported		</div>
+  </div>
