b409a78d46444799158b5830fe6a3dc8baf3f93f
John Muli <johnsenga@gmail.com>
2018-04-15 21:11:31 +0300

    fix on accrual

--- jobs/accrualRent.php
+++ jobs/accrualRent.php
@@ -35,7 +35,7 @@ while($row=mysqli_fetch_assoc($sqlres)){
     $status = "1";
     $stmt = $mysqli->prepare("
     INSERT INTO invoices (tenantId,leaseId,amount,dateRaised,dateDue,description,status,invoiceType) VALUES (?,?,?,NOW(),?,?,?,?)");
-    $stmt->bind_param('ssssss',
+    $stmt->bind_param('sssssss',
     $row["userId"],
     $row["leaseId"],
     $row["propertyRate"],
@@ -102,7 +102,7 @@ $stmt->close();
     $status = "1";
     $stmt = $mysqli->prepare("
     INSERT INTO invoices (tenantId,leaseId,amount,dateRaised,dateDue,description,status,invoiceType) VALUES (?,?,?,NOW(),?,?,?,?)");
-    $stmt->bind_param('ssssss',
+    $stmt->bind_param('sssssss',
     $row["userId"],
     $row["leaseId"],
     $row["latePenalty"],
