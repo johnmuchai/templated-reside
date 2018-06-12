291b717ed3b872b1b0cdb76a07cad2581eaf4717
lenny81k <lenny81k@gitlab.com>
2018-06-12 17:02:45 +0300

    my modules 1.Autoupdate leases links: http://localhost/admin/pages/Autupdateleases.php features ## This page should autorun in intervals - Not yet configured to autorun but working on it with cron ## Should gather data from leased properties and place it on paymentpostage table ## Therefore requires you to add a new table called "paymentpostage" - Its here in attached ## Required to add a field by the name "Posted" with INT data type on the properties table. The page reads "0" for unleased    property but updates the posted field with "1" when the property is leased. ## The page auto updates accrued rent amounts for every unpaid for month. ## After running the page, the following accounting module should also be updated automatically ## Will only display expected amounts for the month of july since the system assume rent for june has been made while leasing respective units 2. Accounting module: ## Requires automatic updates from the payment postage table ###Displays the (After the first month of property Leasing) ###Total Expeceted Rent Amount ###Total Collected Rent Amount ###Income on Commission ###Income on Penalties ###Aggregated Income   Kindly format its display alittle bit as am trying to learn how the way the systems formating works   Also do som improvements where required though am not done with it yet. 3. Requires the attached paymentpostage.sql table
    Signed-off-by: lenny81k <lenny81k@gitlab.com>

---
+++ admin/pages/accountsmoddle.php
@@ -0,0 +1,272 @@
+
+
+<?php // php populate html table from mysql database
+
+//include variables;
+$link = mysqli_connect("localhost", "root", "", "reside_prod");
+
+// Check connection
+
+$todyasmonth = date("F", strtotime($sdate));
+if($link === false){
+    die("ERROR: Could not link. " . mysqli_connect_error());
+}
+// Check connection
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+
+$query = "SELECT * FROM `paymentspostage";
+
+// result for method one
+$result1 = mysqli_query($link, $query);
+// result for method two
+$result2 = mysqli_query($link, $query);
+
+$dataRow = "";
+
+while($row2 = mysqli_fetch_array($result2))
+{
+    //$dataRow = $dataRow."<tr><td>$row2[4]</td><td>$row2[2]</td><td>$row2[5]</td><td>$row2[7]<tr><td>$row2[16]]</td></tr>";
+
+	$dataRow = $dataRow."<tr>  <td>$row2[4]</td><td>$row2[2]</td><td>$row2[5]</td><td>$row2[7]</td> <td>$row2[7]</td> <td>$row2[16]</td> </tr>";
+}
+
+
+	$addCss = '<link href="../css/dataTables.css" rel="stylesheet">';
+	   include 'includes/header.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title>PHP DATA ROW TABLE FROM DATABASE</title>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <style>
+
+            table,th,tr,td
+            {
+                border: 1px solid black;
+            }
+
+        </style>
+
+    </head>
+
+    <body>
+
+<!-- Table One -->
+        <table align ="centre">
+
+
+            <?php while($row1 = mysqli_fetch_array($result1)):;?>
+
+            <?php endwhile;?>
+
+        </table>
+
+        <br>
+        <table width="835" border="0">
+          <tr>
+            <td><form name="form1" method="post" action="">
+              <label>Property Name </label>
+
+            </form></td>
+            <td colspan="2"><select name="select">
+            </select></td>
+          </tr>
+          <tr>
+            <td>Date</td>
+            <td colspan="2"><label1  > </label>&nbsp;</td>
+          </tr>
+          <tr>
+            <td width="136">&nbsp;</td>
+            <td colspan="2">&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td colspan="2"><table style="">
+              <tr>
+                <th width="97">Unit Name</th>
+                <th width="183">Tenant's Name</th>
+                <th width="103">Rent Amount</th>
+                <th width="72">Penalties</th>
+                <th width="97">Commission</th>
+                <th width="101">Rent Balance</th>
+              </tr>
+
+			  <input type="hidden" id="weekStart" value="<?php echo $set['weekStart']; ?>" />
+
+				<div class="row mb-20">
+					<div class="col-md-3">
+
+
+
+
+              <?php echo $dataRow;?>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><div align="right">Total Expeceted Amount : </div></td>
+            <td>&nbsp;
+
+			<?php
+				$sql = "SELECT SUM(RentBalance ) AS value_sum FROM paymentspostage";
+			$result = $link->query($sql);
+
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+
+					$expectectedamounts = $row["value_sum"];
+					echo $expectectedamounts;
+				}
+			} else {
+				echo "0 results";}
+
+				?>
+
+
+			<label>
+
+			<?php
+//$itemName = 'pencil';
+//$variable = 'itemName';
+//echo "<span class=\"label-$variable\">${$variable}</span>";
+//Prints: <span class="label-itemName
+?>
+
+			</label></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><div align="right">Total Collected Amount : </div></td>
+            <td>&nbsp;
+
+			<?php
+					$sql = "SELECT SUM(Rent_Amount) AS value_sum FROM paymentspostage";
+				$result = $link->query($sql);
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) {
+
+					$collectedamount = 0;
+					echo $collectedamount;
+					}
+				} else {
+					echo "0 results";}
+
+					?>
+
+
+
+
+			<label></label></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><div align="right">Income on Commission : </div></td>
+            <td>
+				<?php
+	$sql = "SELECT SUM(RentBalance) AS value_sum FROM paymentspostage";
+$result = $link->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      //  echo  $row["value_sum"];
+		$expectedamount = $row["value_sum"];
+		$value = 7;
+		$percentage = 100;
+
+			$percentagevalue = $value / $percentage ;
+			$percentagecommmission = $percentagevalue * $expectedamount ;
+	     	echo $percentagecommmission ;
+    }
+} else {
+    echo "0 results";}
+	?>
+
+
+
+			&nbsp;<label></label></td>
+          </tr>
+          <tr>
+
+
+            <td>&nbsp;</td>
+            <td><div align="right">Income on Penalties : </div></td>
+            <td>
+
+				<?php
+	$sql = "SELECT SUM(Penalties) AS value_sum FROM paymentspostage";
+$result = $link->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $penalties = $row["value_sum"];
+		echo  $penalties ;
+    }
+} else {
+    echo "0 results";}
+
+
+	?>
+
+
+
+
+			&nbsp;<label></label></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td width="177"><div align="right">Aggregated Income : </div></td>
+            <td width="500">
+
+						<?php
+			$totalcollectedamount = $collectedamount - $expectectedamounts ;
+			$totalincome = $percentagecommmission + $penalties ;
+			$Aggreagtedincome1 = $totalcollectedamount + $totalincome ;
+
+			?>
+
+			&nbsp;</td>
+          </tr>
+    </table>
+        <br>
+
+ <!-- Table Two -->
+    </body>
+
+</html>
+<?php
+
+$sql = "SELECT * FROM paymentspostage";// WHERE isLeased ='1' AND Posted=$Propertypostingvar";
+if($result11 = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result11) > 0){
+		do {
+		//$smonth = $row['S_month'];
+		echo $smonth ;
+		}
+						while ($row = mysqli_fetch_array($result11));
+
+        mysqli_free_result($result11);
+		///die ;
+    } else{
+        echo "No records matching your query were found.";
+
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql10. " . mysqli_error($link);
+}
+
+?>
